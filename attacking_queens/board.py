323d2c2e358a33b433bbb9ca31afe856da0833ec
Kristijan Spirovski <k_spirovski@hotmail.com>
2018-04-02 13:43:03 +0200

    number of available black pieces.

--- attacking_queens/board.py
+++ attacking_queens/board.py
@@ -1,3 +1,4 @@
 class ChessBoard:
     def __init__(self, size: int):
         self.size = size
+        self.black_size = int(size * size / 2) + 1
--- attacking_queens/tests/test_attacking_queens.py
+++ attacking_queens/tests/test_attacking_queens.py
@@ -5,7 +5,12 @@ from attacking_queens.board import ChessBoard
 
 class ChessBoardTests(unittest.TestCase):
 
-    def test_chess_board(self):
+    def test_chess_board_size(self):
         board = ChessBoard(size=5)
 
         self.assertEqual(board.size, 5)
+
+    def test_chess_board_available_black_places(self):
+        board = ChessBoard(size=5)
+
+        self.assertEqual(board.black_size, int(5 * 5 / 2) + 1)
