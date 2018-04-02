848e5767bf79d473e6bc57fa998c22bd70cd1b99
Kristijan Spirovski <k_spirovski@hotmail.com>
2018-04-02 13:59:08 +0200

    Refactoring board, extracting white_size and black_size as properties of ChessBoard class.

--- attacking_queens/board.py
+++ attacking_queens/board.py
@@ -1,5 +1,11 @@
 class ChessBoard:
     def __init__(self, size: int):
         self.size = size
-        self.white_size = int(size * size / 2)
-        self.black_size = self.size ** 2 - self.white_size
+
+    @property
+    def white_size(self):
+        return int(self.size ** 2 / 2)
+
+    @property
+    def black_size(self):
+        return self.size ** 2 - self.white_size
