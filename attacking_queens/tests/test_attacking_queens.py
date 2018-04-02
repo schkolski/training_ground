10692f838b5c2ff0b7e050b5d4f60afc67986a7d
Kristijan Spirovski <k_spirovski@hotmail.com>
2018-04-02 13:47:37 +0200

    simple refactoring in ChessBoardTests

--- attacking_queens/tests/test_attacking_queens.py
+++ attacking_queens/tests/test_attacking_queens.py
@@ -5,17 +5,14 @@ from attacking_queens.board import ChessBoard
 
 class ChessBoardTests(unittest.TestCase):
 
-    def test_chess_board_size(self):
-        board = ChessBoard(size=5)
+    def setUp(self):
+        self.board = ChessBoard(size=5)
 
-        self.assertEqual(board.size, 5)
+    def test_chess_board_size(self):
+        self.assertEqual(self.board.size, 5)
 
     def test_chess_board_available_black_places(self):
-        board = ChessBoard(size=5)
-
-        self.assertEqual(board.black_size, int(5 * 5 / 2) + 1)
+        self.assertEqual(self.board.black_size, 13)
 
     def test_chess_board_available_white_places(self):
-        board = ChessBoard(size=5)
-
-        self.assertEqual(board.white_size, int(5 * 5 / 2))
+        self.assertEqual(self.board.white_size, 12)
